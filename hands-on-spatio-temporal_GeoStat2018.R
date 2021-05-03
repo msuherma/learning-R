2a2667a624151dee37a411354d5626bd1cd2aebd
msuherma <msuherma@purdue.edu>
2021-05-03 08:21:10 -0400

    adding some exercise

---
+++ hands-on-spatio-temporal_GeoStat2018.R
@@ -0,0 +1,15 @@
+# This code is derived from the exercises found in : Geostat2018 - HannaMeyer
+# github:https://github.com/HannaMeyer/Geostat2018
+# Youtube: https://www.youtube.com/watch?v=5NQUae1DzgU&t=109s
+
+#############################################
+#     Hands-on Machine Learning based Modeling of Spatial
+#     and Spatio-temporal data
+#############################################
+# rewritten by: ckhrmn
+
+
+library(raster)
+library(caret)
+library(mapview)
+library(sf)
